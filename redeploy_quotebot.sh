c92ec8a89f7b0507e4ac74795181373b380e7633
Charles Zhang <zhangchuck@users.noreply.github.com>
2020-09-16 22:24:20 -0700

    Add additional environment variables to script

--- redeploy_quotebot.sh
+++ redeploy_quotebot.sh
@@ -15,11 +15,15 @@ then
 	then
 		docker run --restart unless-stopped \
 	    		-d -e DISCORD_QUOTEBOT_TOKEN=$DISCORD_QUOTEBOT_TOKEN \
+			-e DISCORD_QUOTEBOT_BUCKET=$DISCORD_QUOTEBOT_BUCKET \
+			-e DISCORD_QUOTEBOT_DB_FILENAME=$DISCORD_QUOTEBOT_DB_FILENAME \
 			cyzhang/discord_quote_bot:latest
 	elif [ "$_temp_tag" == "development" ]
 	then
 		docker run --restart unless-stopped \
 	    		-d -e DISCORD_QUOTEBOT_TOKEN=$DISCORD_QUOTEBOT_DEV_TOKEN \
+			-e DISCORD_QUOTEBOT_BUCKET=$DISCORD_QUOTEBOT_BUCKET_DEV \
+			-e DISCORD_QUOTEBOT_DB_FILENAME=$DISCORD_QUOTEBOT_DB_FILENAME_DEV \
 			cyzhang/discord_quote_bot:development
 	fi
 	
